4aa3e0fcfb225f4d3aa1fa13e3b892d6ee64f02c
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-14 17:17:48 -0500

    intial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+(function() {
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Azurite',
+    price: 110.50,
+    canPurchase: false,
+    soldOut: true
+  };
+})();
